4ed1cc8c6deef24cd9954b7fc1a6a3a7a7e274d9
mays <mailsyamk@gmail.com>
2015-08-30 23:47:16 +0530

    Update Util.rb

--- Util.rb
+++ Util.rb
@@ -68,5 +68,56 @@ module Util
         return false
       end
    end
+   
+   def Util.getSortedSprints(server, endpoint, params)
+        sprints = []
+        sname_by_sequence = {}
+        tseq = []
+
+        newParam = {
+            :action => 'listcustomcontext',
+            :fields => 'sequence, sprint_id',
+            :group_by => 'sequence',
+            :count => '*'
+        }
+
+        newParam = params.merge(newParam)
+        cqlQuery = CloudmunchService.getDataContext(server, endpoint, newParam)
+        cqlQuery = JSON.parse(cqlQuery)
+        cqlQuery.each do |v|
+            # puts v[1]
+            tseq << v[1]['sequence'].to_i
+            sname_by_sequence[v[1]['sequence'].to_i] = v[1]['sprint_id']
+        end
+
+        tseq.sort!.each do |y|
+            sprints << sname_by_sequence[y]
+        end
+
+        return sprints
+    end
+
+    def Util.getActiveSprint(server, endpoint, params)
+        sprints = []
+
+        newParam = {
+            :action => "listcustomcontext",
+            :fields => "sprint_id,sprint_status",
+            :sort_by => "sprint_status",
+            :count => "*",
+        }
+
+        newParam = params.merge(newParam)
+        cqlQuery = CloudmunchService.getDataContext(server, endpoint, newParam)
+        cqlQuery = JSON.parse(cqlQuery)
+        sprint = nil
+        cqlQuery.each do |x|
+            if(x['sprint_status'] == 'ACTIVE')
+                sprint = x['sprint_id']
+            end
+        end
+        return sprint
+   end
+    
 end
 
